9d0dbe28ddcb021822a6fdf4b0417c0da21dd720
Anurag Tiwari <anurag1504t@gmail.com>
2020-08-27 11:40:50 +0530

    Corrected Server URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 var dotenv = require("dotenv").config()
 const port = process.env.PORT || 3000;
 module.exports={
-    serverurl: "https://myurc.herokuapp.com/"
+    serverurl: "https://myurc.herokuapp.com"
 }
