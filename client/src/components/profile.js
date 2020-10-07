4f2bec19eb6e1fb3f04f8f1aae5837638fe3866e
adityatandon14 <you@example.com>
2020-10-07 20:31:35 +0530

    imgsize

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -203,7 +203,7 @@ return(
                         <div>                            
                             <div className='dd'>Hi, <em>{pdata.name}</em></div>
                             <br></br>
-                            <img className="profile-image" src={Officer} width="300px" ></img>
+                            <img className="profile-image" src={Officer} width="90%" ></img>
                             <div className='dd'>@{pdata.username}</div>
                             <div className='dd'><span className="fa fa-envelope"></span>  {pdata.email}</div>
                             <div className='dd'><span className="fa fa-phone"></span>  {pdata.mobileNumber}</div>
